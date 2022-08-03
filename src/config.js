315cf1f537fb6609d5082828a59a1d7c254a555c
firizky29 <13520095@std.stei.itb.ac.id>
2022-08-03 23:28:07 +0700

    production

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const BASE_URL = "http://localhost:3100/";
 export const API_URL = `${BASE_URL}api/v1`;
+export const API_KEY = process.env.API_KEY;
+export const API_EXCHANGE_RATE = `https://api.exchangeratesapi.io/latest?access_key=$(API_KEY)`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
+import { API_EXCHANGE_RATE, API_KEY } from '../config';
 
 const Home = (props: any) => {
+    const [symbols, setSymbols] = React.useState([]);
+    const [symbol, setSymbol] = React.useState('IDR');
+
+    useEffect(() => {
+        if(API_KEY){
+            var myHeaders = new Headers();
+            myHeaders.append("apikey", API_KEY);
+
+            fetch("https://api.apilayer.com/exchangerates_data/symbols", 
+                {
+                    method: 'GET',
+                    redirect: 'follow',
+                    headers: myHeaders
+                }
+            )
+                .then(response => response.json())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
+        }
+    }, []);
+
     if (!props.isLoggedIn) {
         return <Navigate replace to="/login" />
     } else {
@@ -15,7 +38,7 @@ const Home = (props: any) => {
                         <div className="card-body">
                             <div className="form-group d-flex justify-content-around">
                                 <div className="custom-control custom-radio">
-                                    <input className="custom-control-input" type="radio" id="customRadio1" name="customRadio" checked />
+                                    <input className="custom-control-input" type="radio" id="customRadio1" name="customRadio" />
                                     <label htmlFor="customRadio1" className="custom-control-label">Debit</label>
                                 </div>
                                 <div className="custom-control custom-radio">
@@ -25,9 +48,21 @@ const Home = (props: any) => {
                             </div>
                             <div className="input-group">
                                 <div className="input-group-prepend">
-                                    <button type="button" className="btn btn-default dropdown-toggle dropdown-icon" data-toggle="dropdown">
-                                        <span>$ </span>
-                                    </button>
+                                    <div className="btn-group align-self-center align-top mr-2">
+                                        <DropdownButton
+                                            title={symbol}
+                                            id="dropdown-menu-align-right"
+                                            variant="light"
+                                            onSelect={(eventKey: any) => setSymbol(eventKey)}
+                                        >
+                                            {
+                                                Array.isArray(symbols) ?
+                                                    symbols.map((symbol: any, index: number) => {
+                                                        return <Dropdown.Item eventKey={symbol}>{symbol}</Dropdown.Item>
+                                                    }) : null
+                                            }
+                                        </DropdownButton>
+                                    </div>
                                 </div>
                                 <input type="text" className="form-control" />
                                 <div className="input-group-append">
